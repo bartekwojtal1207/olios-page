b1968776753ae318645e2f47c9d48a2c14f8305b
bartekwojtal1207 <bartekwojtal1207@gmail.com>
2017-04-11 17:28:54 +0200

    oliols page
    main page is complited (no fonts and bad color )

--- js/js.js
+++ js/js.js
@@ -8,6 +8,7 @@ $(document).ready(function(){
 
   $("#menu-toggle").click(function(e) {
         e.preventDefault();
+        $("#menu-toggle").toggleClass("clicked");
         $("#wrapper").toggleClass("toggled");
 });
 })
