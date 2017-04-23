53c66fa034ba59f8fe3514b48eb31c7cf72922ac
bartekwojtal1207 <bartekwojtal1207@gmail.com>
2017-04-23 17:56:09 +0200

    olios page (product section)
    upadate slider in product section
    #jquery

--- js/productJs.js
+++ js/productJs.js
@@ -7,17 +7,32 @@ $(document).ready(function(){
     product_counter ++;
     quanity.text(product_counter);
   });
-  var produkt = $(".recomended_product").find("img");
+  var nav_foto = $(".recomended_product").find("img");
 
-  produkt.on("mouseenter",function(){
-    var produkt_selected = $(".show_product").children();
-    var foto_source = $(produkt_selected).attr("src");
+  nav_foto.on("click",function(){
+    var main_foto = $(".show_product").children();
+    main_foto.css("display","none");
+    var title_seat = $(main_foto).data("title");
+    var foto_source = $(main_foto).attr("src");
     var new_foto = $(this).attr("src");
-    console.log("mainfoto"+foto_source);
-    console.log("drugiefoto"+new_foto);
-    produkt_selected.attr("src",new_foto);
+    var product_title = $(this).data("title");
+    var title_page = $(".title_description");
+    main_foto.attr("src",new_foto);
+    main_foto.data("title",product_title);
+    $(this).next().text(title_seat);
+    title_page.text(product_title);
     $(this).attr("src",foto_source);
-
+    $(this).data("title",title_seat);
+    main_foto.fadeIn(2000);
+//    main_foto.data("title");
+    console.log(product_title);
+    console.log(title_seat);
+  });
+  nav_foto.on("mouseenter",function(){
+    $(this).css("opacity","0.5");
+  });
+  nav_foto.on("mouseleave",function(){
+    $(this).css("opacity","1");
   });
 
 });
