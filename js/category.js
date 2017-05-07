91dead0931976fcc8fac5fa6254aff9eaf2a662d
bartekwojtal1207 <bartekwojtal1207@gmail.com>
2017-05-07 22:43:05 +0200

    olios page
    category section
    #add googlemaps to category page

--- js/category.js
+++ js/category.js
@@ -1,6 +1,7 @@
 $(document).ready(function(){
+
   var show_more_link = $(".footer_category");
-  $(".map_box").css("display","none");
+  //$(".map_box").css("display","none");
   $(show_more_link).on("click",function(){
       $(".map_box").slideToggle(1000);
   });
