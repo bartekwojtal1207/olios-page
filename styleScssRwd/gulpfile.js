9d6adfdaadd236b5d4f1db111861cbc03842c585
bartekwojtal1207 <bartekwojtal1207@gmail.com>
2017-05-16 19:49:51 +0200

    olios page
    add rwd for all section

--- styleScssRwd/gulpfile.js
+++ styleScssRwd/gulpfile.js
@@ -1,15 +1,15 @@
 var gulp = require("gulp");// say to node.js that conect to gulp in folder npm install
-var jshint = require('gulp-jshint');
+// var jshint = require('gulp-jshint');
 var sass = require('gulp-sass');
 
-  gulp.task('jshint', function() {
-    return gulp.src('js/*.js').pipe(jshint()).pipe(jshint.reporter('default'));
-  });
+  // gulp.task('jshint', function() {
+  //   return gulp.src('js/*.js').pipe(jshint()).pipe(jshint.reporter('default'));
+  // });
   gulp.task('sass', function() {
     return gulp.src('styleScss/style.scss').pipe(sass({errLogToConsole: true})).pipe(gulp.dest('css'));
   });
   gulp.task('sassRwd', function() {
-    return gulp.src('styleScssRwd/style.scss').pipe(sass({errLogToConsole: true})).pipe(gulp.dest('cssRwd'));
+    return gulp.src('scssRWd/styleRwd.scss').pipe(sass({errLogToConsole: true})).pipe(gulp.dest('cssRwd'));
   });
   gulp.task('watch', function(){
     gulp.watch('styleScss/*.scss', ['sass']);
